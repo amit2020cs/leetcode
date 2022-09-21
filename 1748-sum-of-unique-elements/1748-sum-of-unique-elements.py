5bd27c23e1f7fb28abe8ac91ccddaf48dcfbea2d
Amit Singh <31853896+amit2020cs@users.noreply.github.com>
2022-09-21 08:24:21 +0530

    Time: 69 ms (21.13%), Space: 13.9 MB (9.35%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
@@ -1,13 +1,14 @@
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
-        dic = {}
-        for i in nums:
-            if i not in dic:
-                dic[i]=1
-            else:
-                dic[i]+=1
-        result = 0
-        for key, val in dic.items():
-            if val == 1:
-                result += key
-        return result       
+        # dic = {}
+        # for i in nums:
+        #     if i not in dic:
+        #         dic[i]=1
+        #     else:
+        #         dic[i]+=1
+        # result = 0
+        # for key, val in dic.items():
+        #     if val == 1:
+        #         result += key
+        # return result       
+        return sum([number for number, count in Counter(nums).items() if count == 1])
