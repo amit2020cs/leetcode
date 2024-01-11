f52e9aa65ad62cc5de26b85a519195ed26a0b03b
Amit Singh <31853896+amit2020cs@users.noreply.github.com>
2024-01-12 00:41:18 +0530

    Time: 45 ms, Memory: 17.4 MB - LeetHub

---
+++ bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rangeBitwiseAnd(self, m: int, n: int) -> int:
+        while m < n:
+            # turn off rightmost 1-bit
+            n = n & (n - 1)
+        return m & n
+        
