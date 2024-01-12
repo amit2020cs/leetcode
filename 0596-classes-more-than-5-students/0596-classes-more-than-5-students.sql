34b4c35249550592ce7961f13d828d604a8f29fc
Amit Singh <31853896+amit2020cs@users.noreply.github.com>
2024-01-12 21:04:09 +0530

    Time: 597 ms (42.79%), Space: 0B (100.00%) - LeetHub

--- 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
+++ 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 select class
-from (
-select class , count(student) as num from Courses
-group by class) as temp
-where num >=5
+from courses
+group by class
+having count(student) >=5
