9bc7af8f3ccf23d1021f85595884ca7c608616aa
Amit Singh <31853896+amit2020cs@users.noreply.github.com>
2024-01-14 10:25:16 +0530

    Time: 131 ms (71.65%), Space: 18.4 MB (28.83%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -1,14 +1,12 @@
-from collections import Counter
-
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         if len(word1) != len(word2):
             return False
-        frequency1 = Counter(word1)
-        frequency2 = Counter(word2)
-        if set(frequency1.keys()) != set(frequency2.keys()):
-            return False
-        frequency1 = dict(sorted(frequency1.items(), key=lambda item: item[1]))
-        frequency2 = dict(sorted(frequency2.items(), key=lambda item: item[1]))
-        return [frequency for frequency in frequency1.values()] == [frequency for frequency in frequency2.values()]          
+        feq1 = Counter(word1)
+        feq2 = Counter(word2)
         
+        if set(feq1.keys()) != set(feq2.keys()):
+            return False
+        feq1 = dict(sorted(feq1.items(), key= lambda item: item[1]))
+        feq2 = dict(sorted(feq2.items(), key= lambda item: item[1]))
+        return [feq for feq in feq1.values()] == [feq for feq in feq2.values()]       
