4a5779faa5f1a7d9a4f2f806f1502e8ff713ce6e
Amit Singh <31853896+amit2020cs@users.noreply.github.com>
2024-01-26 00:15:39 +0530

    Time: 45 ms (59.83%), Space: 16.5 MB (65.27%) - LeetHub

--- 0360-sort-transformed-array/0360-sort-transformed-array.py
+++ 0360-sort-transformed-array/0360-sort-transformed-array.py
@@ -1,7 +1,12 @@
 class Solution:
     def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
+        
+        def function(x, a, b,c ):
+            
+            res = a*(x*x) + b*x +c
+            return res
         ans = []
-        for x in nums:
-            ans.append((a*x*x) + (b*x) +c)
+        for i in nums:
+            ans.append(function(i,a,b,c))
         ans.sort()
         return ans
