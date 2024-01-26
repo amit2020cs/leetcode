bd2b8d59ffcbd4aa1681a4c24dcac30a62cdedee
Amit Singh <31853896+amit2020cs@users.noreply.github.com>
2024-01-26 17:19:09 +0530

    Time: 86 ms (98.81%), Space: 21.1 MB (59.22%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.py
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.py
@@ -25,21 +25,52 @@ class Solution:
             prevptr.next = None
         return slowptr
     
+    def maptolist(self, head):
+        value = []
+        
+        while head:
+            value.append(head.val)
+            head = head.next
+        
+        return value
+    
     def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
-        
-        if not head:
-            return None
-        
-        mid = self.findMid(head)
-        
-        node = TreeNode(mid.val)
-        
-        if head == mid:
+        values = self.maptolist(head)
+
+        # l and r represent the start and end of the given array
+        def convertListToBST(l, r):
+
+            # Invalid case
+            if l > r:
+                return None
+
+            # Middle element forms the root.
+            mid = (l + r) // 2
+            node = TreeNode(values[mid])
+
+            # Base case for when there is only one element left in the array
+            if l == r:
+                return node
+
+            # Recursively form BST on the two halves
+            node.left = convertListToBST(l, mid - 1)
+            node.right = convertListToBST(mid + 1, r)
             return node
+        return convertListToBST(0, len(values) - 1)
+            
+#         if not head:
+#             return None
         
-        node.left = self.sortedListToBST(head)
-        node.right = self.sortedListToBST(mid.next)
+#         mid = self.findMid(head)
         
-        return node
+#         node = TreeNode(mid.val)
+        
+#         if head == mid:
+#             return node
+        
+#         node.left = self.sortedListToBST(head)
+#         node.right = self.sortedListToBST(mid.next)
+        
+#         return node
         
         
