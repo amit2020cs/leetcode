29317e7c5e0adcc948ac493dbdb990581fa0bcc1
Amit Singh <31853896+amit2020cs@users.noreply.github.com>
2024-01-26 13:36:34 +0530

    Time: 79 ms (97.56%), Space: 21.2 MB (78.50%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,12 +2,19 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
         hashmap = {}
-        
-        for i in range(len(nums)):
-            if nums[i] in hashmap:
-                hashmap[nums[i]] += 1
+        for i in nums:
+            if i in hashmap:
+                hashmap[i] +=1
             else:
-                hashmap[nums[i]] = 1
+                hashmap[i] = 1
+        
+#         for i in range(len(nums)):
+#             if nums[i] in hashmap:
+#                 hashmap[nums[i]] += 1
+#             else:
+#                 hashmap[nums[i]] = 1
+
+        
         
         # Sort the dictionary items based on frequency in descending order
         sorted_items = sorted(hashmap.items(), key=lambda x: x[1], reverse=True)
