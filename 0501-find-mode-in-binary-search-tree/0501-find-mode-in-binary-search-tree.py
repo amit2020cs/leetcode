52e674a4cc020876c7a2cb755883ac46247b105b
Amit Singh <31853896+amit2020cs@users.noreply.github.com>
2024-01-26 11:18:40 +0530

    Time: 44 ms (88.69%), Space: 19.8 MB (94.28%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        
+        counter = defaultdict(int)
+        
+        queue = deque([root])
+        
+        while queue:
+            node = queue.popleft()
+            
+            counter[node.val]+=1
+            
+            if node.left:
+                queue.append(node.left)
+            
+            if node.right:
+                queue.append(node.right)
+                
+        max_freq = max(counter.values())
+        
+        ans = []
+        
+        for key in counter:
+            if counter[key] == max_freq:
+                ans.append(key)
+        return ans
+            
