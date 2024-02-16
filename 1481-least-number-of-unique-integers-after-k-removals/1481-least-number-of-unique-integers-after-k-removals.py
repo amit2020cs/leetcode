d29dacc1bd51f24f056bb6464c130066f272aa6f
Amit Singh <31853896+amit2020cs@users.noreply.github.com>
2024-02-16 07:50:49 +0530

    Time: 318 ms (90.64%), Space: 33.4 MB (84.81%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
@@ -1,16 +1,8 @@
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
         
-        hashmap = {}
-        
-        for i in arr:
-            if i not in hashmap:
-                hashmap[i]=1
-            else:
-                hashmap[i]+=1
-        
-        frequency = list(hashmap.values())
-        
+        frequency_map = Counter(arr)
+        frequency = list(frequency_map.values())
         frequency.sort()
         ele_removed = 0
         for i in range(len(frequency)):
